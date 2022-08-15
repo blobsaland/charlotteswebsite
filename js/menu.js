8ad22fb41aefbd2fc14145caad83664835879c45
blobsaland <68444206+blobsaland@users.noreply.github.com>
2022-08-15 16:15:09 +0200

    php->html

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+document.getElementById("menu").innerHTML =
+        '<input class="menu-btn" type="checkbox" id="menu-btn"/>'
+        +'<label class="menu-icon" for="menu-btn"><p class="navicon">+</p></label>'
+        +'<div class="menu">'
+                +'<h3><span class="star">★</span><a href="home.html" class="home">Disco Hub</a></h3>'
+                +'<h3><span class="star">★</span><a href="portfolio.html" class="">Portfolio</a></h3>'
+                +'<h3><span class="star">★</span><a href="dj.html" class="">DJ</a></h3>'
+                +'<h3><span class="star">★</span><a href="about-contact.html" class="">About / Contact</a></h3>'
+        +'</div>'
+
+
+
+
